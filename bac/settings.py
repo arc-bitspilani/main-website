c169d925987452fd4b811cf1bb75aac89beb9ae4
Aakash Bajaj <aakashbajaj5311@gmail.com>
2017-10-04 15:03:37 +0530

    Connect mysql

--- bac/settings.py
+++ bac/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import secret as datadb
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,7 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pending_registrations',
-        'USER': datadb.MYSQL_USER
+        'USER': datadb.MYSQL_USER,
+        'PASSWORD': datadb.MYSQL_PASS,
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
