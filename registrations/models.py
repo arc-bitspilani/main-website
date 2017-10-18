e042a3bf301a56df73436999f43559d93c153c3e
Aakash Bajaj <aakashbajaj5311@gmail.com>
2017-10-18 17:55:23 +0530

    Changed database

--- registrations/models.py
+++ registrations/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+from datetime import datetime as dt
+
+
+class Alumni(models.Model):
+	fname = models.CharField("First Name", max_length=200, null=False, blank=False),
+	mname = models.CharField("Middle Name", max_length=200, null=True, blank=True),
+	lname = models.CharField("Last Name", max_length=200, null=False, blank=False),
+	
