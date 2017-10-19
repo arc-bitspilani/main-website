d75332f523c7ebe6174bfab689d0b1fdf1038df7
Keshav Joshi <keshavjoshi142@gmail.com>
2017-10-19 11:33:46 +0530

    x

--- registrations/views.py
+++ registrations/views.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Index")
+	 	return render(request,'../templates/registrations/index.html')
