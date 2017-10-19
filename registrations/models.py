0af41926b687d25af4f017de77de047a89b8d41e
Aakash Bajaj <aakashbajaj5311@gmail.com>
2017-10-19 23:02:46 +0530

    Changed model

--- registrations/models.py
+++ registrations/models.py
@@ -21,8 +21,6 @@ class Alumni(models.Model):
 	)
 	user = models.OneToOneField(User, null=True, blank=True)
 	name = models.CharField("Name", max_length=200)
-	fname = models.CharField("First Name", max_length=200)
-	lname = models.CharField("Last Name", max_length=200)
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone_no = models.CharField(max_length=200, validators=[phone_regex])
 	# email_regex = RegexValidator(regex=r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", message="Invalid Email Format")
